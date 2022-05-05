af7fdee5c40449bee7f2080efe93e2a6bf21c3e6
YusukeIwaki <iwaki@i3-systems.com>
2022-05-05 14:35:31 +0900

    fix updatekeyValue to insert key/value pair when no key matches

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,21 +1,20 @@
 import { equal } from 'assert'
 import { updateKeyValue } from './util'
 
-test('replace with predicate and valueTranslator', () => {
-    const target: Array<[string, number]> = [["key1", 10], ["Key2", 20], ["key3", 30]]
-    updateKeyValue(target,
-        (a: string) => a.startsWith('k'),
-        (a: number) => a * a,
-    )
-    expect(target[0]).toEqual(["key1", 100]);
-    expect(target[1]).toEqual(["Key2", 20]);
-    expect(target[2]).toEqual(["key3", 900]);
-})
-
 test('replace with string and value', () => {
     const target: Array<[string, number]> = [["key1", 10], ["Key2", 20], ["key3", 30]]
     updateKeyValue(target, 'key1', 1000)
     expect(target[0]).toEqual(["key1", 1000]);
     expect(target[1]).toEqual(["Key2", 20]);
     expect(target[2]).toEqual(["key3", 30]);
+    expect(target[3]).toBeUndefined()
+})
+
+test('add key/value when predicate does not match', () => {
+    const target: Array<[string, number]> = [["key1", 10], ["Key2", 20], ["key3", 30]]
+    updateKeyValue(target, 'key4', 1000)
+    expect(target[0]).toEqual(["key1", 10]);
+    expect(target[1]).toEqual(["Key2", 20]);
+    expect(target[2]).toEqual(["key3", 30]);
+    expect(target[3]).toEqual(["key4", 1000]);
 })
--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,14 @@
-type PredicateFunction<T> = (key: T) => boolean
-type ValueTranslator<T> = (value: T) => T
-export function updateKeyValue<T>(keyValueArray: Array<[string, T]>, predicate: string | PredicateFunction<string>, newValue: T | ValueTranslator<T>) {
-    const keyPredicate = (typeof predicate === 'function') ? predicate as PredicateFunction<string> : (key: string) => key == predicate
-    const valueTranslator = (typeof newValue === 'function') ? newValue as ValueTranslator<T> : (value: T) => newValue
+export function updateKeyValue<T>(keyValueArray: Array<[string, T]>, targetKey: string, newValue: T) {
     // Scan and replace key-value pairs.
+    let found = false
     for (let index = 0; index < keyValueArray.length; index++) {
         const [key, value] = keyValueArray[index];
-        if (keyPredicate(key)) {
-            keyValueArray.splice(index, 1, [key, valueTranslator(value)])
+        if (key == targetKey) {
+            keyValueArray.splice(index, 1, [key, newValue])
+            found = true
         }
+    }
+    if (!found) {
+        keyValueArray.push([targetKey, newValue])
     }
 }
